a4a765bba57f236f3b94acdd7ca8657ae0309ab6
Juan Armando Barrón Lugo <jbarron@tamps.cinvestav.mx>
2021-04-30 16:13:30 -0500

    Detect failure in BB

---
+++ BuildingBlocks/Acq/app/Postman.py
@@ -0,0 +1,115 @@
+from threading import Thread
+import time
+import requests as api #for APIs request
+import logging
+import json
+from TPS.Builder import Builder #TPS API BUILDER
+
+class postman(Thread):
+
+    def __init__(self,API_GATEWAY,GATEWAYS_LIST,SERVICE,SERVICE_IP,SERVICE_PORT,NETWORK,TPSHOST):
+        super(postman, self).__init__()
+        self.LOGER = logging.getLogger()
+        ####
+        self.API_GATEWAY=API_GATEWAY
+        self.GATEWAYS_LIST=GATEWAYS_LIST
+        self.SERVICE=SERVICE
+        self.SERVICE_IP=SERVICE_IP
+        self.SERVICE_PORT=SERVICE_PORT
+        self.NETWORK=NETWORK
+        self.TPSHOST=TPSHOST
+        ##################
+        self.RN=None
+        self.id_service = None
+        self._running=True
+
+    def init_service(self):
+        try:
+            if self.SERVICE is not None and self.SERVICE_IP is not None and self.SERVICE_PORT is not None:
+                ToSend=json.dumps({"SERVICE":self.SERVICE,"IP":self.SERVICE_IP,"PORT":int(self.SERVICE_PORT),"NETWORK":self.NETWORK})
+                url = 'http://%s/ADD' % self.API_GATEWAY
+                headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+                res =api.post(url, data=ToSend,headers=headers).json()
+                self.LOGER.error("INIT")
+            else:
+                self.LOGER.error("NOT ENOUGH INFO TO INIT")
+        except Exception as e:
+            self.LOGER.error("ERROR: %s" % e)
+            self.LOGER.error("BAD PARAMETERS")
+            exit(1)
+
+    def Select_gateway(self):
+        for ag in self.GATEWAYS_LIST:
+            try:
+                url = 'http://%s/health' % ag
+                headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+                res =api.get(url,headers=headers).json()
+                if res['status']=="OK":
+                    self.API_GATEWAY = ag
+                    return 0        
+            except Exception:
+                pass
+        self.API_GATEWAY=None
+        return 1
+
+
+    def IndexData(self,RN,id_service,data):
+        WORKSPACENAME = "SERVICEMESH"
+        INDEXER=Builder(WORKSPACENAME,TPS_manager_host=self.TPSHOST) #api for index data
+        try:
+            INDEXER.TPSapi.format_single_query("notImportant")
+        except AttributeError:
+            INDEXER=Builder(WORKSPACENAME,TPS_manager_host=self.TPSHOST) #api for index data
+
+        data_label = "%s_%s" % (RN,id_service) #this is the name of the document (mongo document)
+        query = INDEXER.TPSapi.format_single_query("notImportant") #not important line. Well yes but actually no.
+        res = INDEXER.TPSapi.TPS(query,"indexdata",workload=data,label=data_label) #this service (getdata) let me send json data and save it into a mongo DB
+        return data_label
+
+    def WarnGateway(self,ToSend):
+        ToSend=json.dumps(ToSend)
+        while(True):
+            try:
+                url = 'http://%s/WARN' % self.API_GATEWAY
+                headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+                res =api.post(url, data=ToSend,headers=headers).json()
+                return res
+            except Exception as ex:
+                self.Select_gateway()
+                if self.API_GATEWAY is None:
+                    return {"status":"ERROR","info":"ERROR"}
+
+
+    def AskGateway(self,ToSend):
+        ToSend=json.dumps(ToSend)
+        while(True):
+            try:
+                url = 'http://%s/ASK' % self.API_GATEWAY
+                headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+                res = api.post(url, data=ToSend,headers=headers).json()
+                return res
+            except Exception as ex:
+                self.Select_gateway()
+                if self.API_GATEWAY is None:
+                    return {"status":"ERROR","info":"ERROR"}
+
+        #------------------#
+
+    def HealthCheck(self): 
+        time_interval= 10
+        while self._running:
+            time.sleep(time_interval)
+            ToSend = {'status':"RUNNING","message":"Health check","label":'',"id":self.id_service,"type":'', "index":'',"control_number":self.RN}
+            self.LOGER.error(ToSend)
+            self.WarnGateway(ToSend)
+
+    def run(self):
+        self.HealthCheck()
+
+    def terminate(self):
+        self._running=False
+
+    def Set_IdService(self,id_service):
+        self.id_service=id_service
+    def Set_RN(self,RN):
+        self.RN=RN
--- BuildingBlocks/TextClassification/app/S.py
+++ BuildingBlocks/TextClassification/app/S.py
@@ -1,7 +1,6 @@
 #import building_middleware as BB ##importing middleware
 import logging #logger
 import socket,os,sys,pickle,json,pandas
-from TPS.Builder import Builder #TPS API BUILDER
 import requests as api #for APIs request
 import time
 import C
@@ -10,93 +9,19 @@ import building_middleware as BB ##importing middleware
 import tempfile
 import tqdm
 import base64
-def init_service():
-    try:
-        if SERVICE is not None and SERVICE_IP is not None and SERVICE_PORT is not None:
-            ToSend=json.dumps({"SERVICE":SERVICE,"IP":SERVICE_IP,"PORT":int(SERVICE_PORT),"NETWORK":NETWORK})
-            url = 'http://%s/ADD' % API_GATEWAY
-            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-            res =api.post(url, data=ToSend,headers=headers).json()
-            LOGER.error("INIT")
-        else:
-            LOGER.error("NOT ENOUGH INFO TO INIT")
-    except Exception as e:
-        LOGER.error("ERROR: %s" % e)
-        LOGER.error("BAD PARAMETERS")
-        exit(1)
+from Postman import postman
 
-def Select_gateway():
-    global API_GATEWAY
-    for ag in GATEWAYS_LIST:
-        try:
-            url = 'http://%s/health' % ag
-            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-            res =api.get(url,headers=headers).json()
-            if res['status']=="OK":
-                API_GATEWAY = ag
-                return 0        
-        except Exception:
-            pass
-    API_GATEWAY=None
-    return 1
+#solutions
+SOLUTIONS_FILE="records.txt"
+f_records=  open(SOLUTIONS_FILE,'a+')
+f_records.close()
 
-
-def IndexData(RN,id_service,data):
-    WORKSPACENAME = "SERVICEMESH"
-    TPSHOST = os.getenv("TPS_MANAGER") 
-    INDEXER=Builder(WORKSPACENAME,TPS_manager_host=TPSHOST) #api for index data
-    try:
-        INDEXER.TPSapi.format_single_query("notImportant")
-    except AttributeError:
-        INDEXER=Builder(WORKSPACENAME,TPS_manager_host=TPSHOST) #api for index data
-
-
-    data_label = "%s_%s" % (RN,id_service) #this is the name of the document (mongo document)
-    query = INDEXER.TPSapi.format_single_query("notImportant") #not important line. Well yes but actually no.
-    res = INDEXER.TPSapi.TPS(query,"indexdata",workload=data,label=data_label) #this service (getdata) let me send json data and save it into a mongo DB
-    return data_label
-
-def WarnGateway(ToSend):
-    global API_GATEWAY
-    ToSend=json.dumps(ToSend)
-    while(True):
-        try:
-            url = 'http://%s/WARN' % API_GATEWAY
-            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-            res =api.post(url, data=ToSend,headers=headers).json()
-            return res
-        except Exception as ex:
-            Select_gateway()
-            if API_GATEWAY is None:
-                return {"status":"ERROR","info":"ERROR"}
-
-
-def AskGateway(ToSend):
-    ToSend=json.dumps(ToSend)
-    while(True):
-        try:
-            url = 'http://%s/ASK' % API_GATEWAY
-            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-            res = api.post(url, data=ToSend,headers=headers).json()
-            return res
-        except Exception as ex:
-            Select_gateway()
-            if API_GATEWAY is None:
-                return {"status":"ERROR","info":"ERROR"}
-
-    #------------------#
 
 def ClientProcess(metadata,data_acq_time):
     os.chdir(BASE_PATH) #in case of error must be set the base path
     DAG = metadata['DAG'] #dag
     data = metadata['data'] #datos {data,type}
     
-    ######## PATCH ###########################################################################
-    #f = open(data['data'],"rb")
-    #data['data']=f.read().decode()
-    #f.close()
-    ##########################################################################################
-
     service = DAG['service']
     service_name = service
     params = DAG['params']
@@ -107,6 +32,13 @@ def ClientProcess(metadata,data_acq_time):
     index_time=0
     execution_time=0
     #validate save data param
+
+    ####### PATCH ###########################################################################
+    AG.Set_IdService(id_service)
+    AG.Set_RN(control_number)
+    AG.start()
+    ##########################################################################################
+
     if 'SAVE_DATA' in params:        
         index_opt = params['SAVE_DATA']
     else:
@@ -132,7 +64,6 @@ def ClientProcess(metadata,data_acq_time):
     result = BB.middleware(data,actions,params) #{data,type,status,message}
     execution_time=time.time()-execution_time
     LOGER.error("### INFO: Finishing execution process... %s" % service_name)
-
     del data
 
     #------------------#
@@ -148,7 +79,7 @@ def ClientProcess(metadata,data_acq_time):
 
 
         index_time = time.time() #<--- time flag
-        label = IndexData(control_number,id_service,result) #indexing result data into DB
+        label = AG.IndexData(control_number,id_service,result) #indexing result data into DB
 
         index_time= time.time() - index_time
         result['data'] = temp_filename
@@ -156,13 +87,18 @@ def ClientProcess(metadata,data_acq_time):
         label=False
         LOGER.error("### INFO: skiping index process")
     #------------------#
+    ########################  save solution's results in diary
+    with open(SOLUTIONS_FILE,'a+') as f_records:
+        # id_service, control_number,results path, DAG of childrens
+        f_records.write("%s,%s,%s,%s\n" %(id_service,control_number,result['data'],json.dumps(childrens)))
+    ############################
 
     ####### WARN #######
     ToSend = {'status':result['status'],"message":result['message'],"label":label,"id":id_service,"type":result['type'], "index":index_opt,"control_number":control_number}
     ToSend['times']={"NAME":service_name,"ACQ":data_acq_time,"EXE":execution_time,"IDX":index_time}
-    WarnGateway(ToSend)
+    AG.WarnGateway(ToSend)
     #------------------#
-
+    AG.terminate()
     LOGER.error("### INFO: sending to childrens...")
 
 ###### PATCH
@@ -176,23 +112,15 @@ def ClientProcess(metadata,data_acq_time):
     try:
 
         for child in childrens: #list[]
-
             ##################### HERE 
             if result['status']=="ERROR":
                 result['type'] = "error"
-                LOGER.error("-------------------- FLAG1")
                 f = tempfile.NamedTemporaryFile(suffix=".error",delete=False)
-                LOGER.error("-------------------- FLAG2")
-
                 f.write("Error".encode())
                 fname = f.name
-                LOGER.error("-------------------- FLAG3")
-
                 result['data']=fname
                 f.close()
                 f = open(fname,"rb")
-                LOGER.error("-------------------- FLAG4")
-
             else:
                 f = open(result['data'],"rb") #open result file to send it to another service
             
@@ -202,12 +130,12 @@ def ClientProcess(metadata,data_acq_time):
             
             ######## ASK #######
             ToSend = {'service':child['service'],'network':NETWORK}
-            res = AskGateway(ToSend)
+            res = AG.AskGateway(ToSend)
             #------------------#
             if 'info' in res: #no more nodes
                 LOGER.error("NO NODES FOUND")
                 data= {'label':'','id':child['id'],'index':'','control_number':control_number,'type':'','status':'ERROR','message': 'no available resources found.'}
-                WarnGateway(data)
+                AG.WarnGateway(data)
             else:
                 errors_counter=0
                 ###### SEND #######
@@ -226,12 +154,12 @@ def ClientProcess(metadata,data_acq_time):
                         if errors_counter>Tolerant_errors: #we reach the limit
                             ######## ASK AGAIN #######
                             ToSend = {'service':child['service'],'network':NETWORK,'update':{'id':res['id'],'status':'DOWN',"type":res['type']}} #update status of falied node
-                            res = AskGateway(ToSend)
+                            res = AG.AskGateway(ToSend)
                             #------------------#
                             errors_counter=0 #reset counter 
                             if 'info' in res: #no more nodes
                                 data= {'label':'','id':child['id'],'index':'','control_number':control_number,'type':'','status':'ERROR','message': 'no available resources found: %s attempts.' % str(errors_counter)}
-                                WarnGateway(data)
+                                AG.WarnGateway(data)
                                 break
                             ip = res['ip'];port = res['port']
                             LOGER.error("TRYING A NEW NODE... IP:%s PORT: %s " %(ip,port) )
@@ -254,17 +182,20 @@ def ClientProcess(metadata,data_acq_time):
 BASE_PATH = os.getcwd()
 LOGER = logging.getLogger()
 GATEWAYS_LIST =os.getenv("API_GATEWAY").split(",")
-API_GATEWAY=None
-Select_gateway()
-if API_GATEWAY is None:
-    LOGER.error("BAG GATEWAY")
-    exit(1)
-
 NETWORK=os.getenv("NETWORK")
 SERVICE=os.getenv("SERVICE")
 SERVICE_IP=os.getenv("SERVICE_IP") 
 SERVICE_PORT=os.getenv("SERVICE_PORT") 
+TPSHOST = os.getenv("TPS_MANAGER") 
+
 Tolerant_errors=10 #total of errors that can be tolarated
+
+##### AG communication handler
+AG=postman(None,GATEWAYS_LIST,SERVICE,SERVICE_IP,SERVICE_PORT,NETWORK,TPSHOST)
+
+if AG.Select_gateway() == 1:
+    LOGER.error("BAD GATEWAY")
+    exit(1)
 
 
 HEADERSIZE = 4096
@@ -279,7 +210,7 @@ host_name = socket.gethostname()
 host_ip = socket.gethostbyname(host_name) 
 LOGER.error("SERVER ON! IP: "+host_ip+" PORT: "+str(port)+"\n\n")
 #   init service  #
-init_service()
+AG.init_service()
 serv.listen(Tolerant_errors)
 
 while True:
